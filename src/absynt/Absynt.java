af51b1fc2892d45200a9abec2b6c1a9a1e073e4a
unix01 <msteffen@ifi.uio.no>
2000-05-28 08:14:57 +0000

    Initiale Revision (Test fuer $ENVIRONMENT)

---
+++ src/absynt/Absynt.java
@@ -0,0 +1,19 @@
+package absyn;
+import java.io.Serializable;
+
+
+
+
+abstract public class Absyn implements Cloneable, Serializable {
+    public Location location;
+
+
+
+//----------------------------------------------------------------------
+//	Abstract Syntax for Mist Programs
+//	------------------------------------
+//
+//	$Id: Absynt.java,v 1.1 2000-05-28 08:14:57 unix01 Exp $
+//
+//	$Log: not supported by cvs2svn $
+//---------------------------------------------------------------------
