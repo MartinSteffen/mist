d7d1f7bec156d3870d28dd581ca3e2cc886bdb6f
unix01 <msteffen@ifi.uio.no>
2000-05-28 13:02:16 +0000

    Zwischenzustand

---
+++ src/absynt/TransitionList.java
@@ -0,0 +1,29 @@
+package absynt;
+import java.io.Serializable;
+
+
+/**
+ * Eine List von Transitionen
+ * 
+ * @author Initially provided by Martin Steffen.
+ * @version $Id: TransitionList.java,v 1.1 2000-05-28 13:02:16 unix01 Exp $
+ */
+
+
+public class TransitionList  extends Absyn implements  java.Enumeration { 
+  
+}
+
+
+
+
+//----------------------------------------------------------------------
+//	Abstract Syntax for Mist Programs
+//	------------------------------------
+//
+//	$Id: TransitionList.java,v 1.1 2000-05-28 13:02:16 unix01 Exp $
+//
+//	$Log: not supported by cvs2svn $
+//	
+//---------------------------------------------------------------------
+
