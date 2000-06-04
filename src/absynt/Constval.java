f0bf0166eb64e0ce95c8498a61ca6e0d15a78a7e
unix01 <msteffen@ifi.uio.no>
2000-06-04 07:39:43 +0000

    Die Klasse ist _nicht mehr abstrakt_! Die Klassen I_val und B_val sind nun ueberfluessig. Integer und boolesche konstante Werte werden als polymorph als Objekte gespeichert. [ms]

--- src/absynt/Constval.java
+++ src/absynt/Constval.java
@@ -6,12 +6,28 @@ import java.io.Serializable;
  * Konstante Werte (Integers und Boolesche Werte)
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: Constval.java,v 1.2 2000-05-28 12:57:09 unix01 Exp $
+ * @version $Id: Constval.java,v 1.3 2000-06-04 07:39:43 unix01 Exp $
  */
 
 
-public abstract class Constval  extends Absyn implements Serializable { 
+public class Constval  extends Absyn implements Serializable { 
+  /**
+     Als konstante Werte sind Integers und boolesche Werte.
+     Diese uebernehmen wir direkt aus Java.
+   */
 
+  public Object val;
+  
+  /**
+   * 2 uberladene Konstruktoren. Gespeichert wird der Wert
+   * als Objekt. 
+   */
+  public Constval (boolean v) {
+    val = new Boolean (v);}
+
+
+  public Constval (int v) {
+    val = new Integer (v);}
 }
 
 
@@ -21,9 +37,12 @@ public abstract class Constval  extends Absyn implements Serializable {
 //	Abstract Syntax for Mist Programs
 //	------------------------------------
 //
-//	$Id: Constval.java,v 1.2 2000-05-28 12:57:09 unix01 Exp $
+//	$Id: Constval.java,v 1.3 2000-06-04 07:39:43 unix01 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  2000/05/28 12:57:09  unix01
+//	Zwischenzustand, vor Reorganisation
+//	
 //	Revision 1.1  2000/05/28 11:11:23  unix01
 //	ok
 //	
