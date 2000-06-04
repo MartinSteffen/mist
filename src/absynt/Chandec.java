ed8aedfa3557fcf1cf85701407076e5c9dd16978
unix01 <msteffen@ifi.uio.no>
2000-06-04 09:50:01 +0000

    Definition geaendert: chan ist nun vom Typ Channel!
    Grund: vielleicht soll die Channeldeclaration nicht
    nur aus dem String bestehen, sondern noch weitere
    Informationen tragen. Die Syntax soll das bereits
    beruecksichtigen. [Steffen]

--- src/absynt/Chandec.java
+++ src/absynt/Chandec.java
@@ -6,14 +6,14 @@ import java.io.Serializable;
  * Variablendeklaration.
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: Chandec.java,v 1.2 2000-06-04 06:34:54 unix01 Exp $
+ * @version $Id: Chandec.java,v 1.3 2000-06-04 09:50:01 unix01 Exp $
  */
 
 
 public class Chandec extends Absyn implements Serializable { 
-  public String chan;    // Name als String
+  public Channel chan;     // 
 
-  public Chandec (String s) {chan = s;};
+  public Chandec (Channel c) {chan = c;};
 }
 
 
@@ -23,9 +23,12 @@ public class Chandec extends Absyn implements Serializable {
 //	Abstract Syntax for Mist Programs
 //	------------------------------------
 //
-//	$Id: Chandec.java,v 1.2 2000-06-04 06:34:54 unix01 Exp $
+//	$Id: Chandec.java,v 1.3 2000-06-04 09:50:01 unix01 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  2000/06/04 06:34:54  unix01
+//	Konstruktor/Feld hinzugefuegt
+//	
 //	Revision 1.1  2000/05/28 15:11:15  unix01
 //	ok
 //	
