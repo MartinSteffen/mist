24616173925e9916197b5d0aa3161910ce76d576
unix01 <msteffen@ifi.uio.no>
2000-06-04 06:34:54 +0000

    Konstruktor/Feld hinzugefuegt

--- src/absynt/Chandec.java
+++ src/absynt/Chandec.java
@@ -6,11 +6,14 @@ import java.io.Serializable;
  * Variablendeklaration.
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: Chandec.java,v 1.1 2000-05-28 15:11:15 unix01 Exp $
+ * @version $Id: Chandec.java,v 1.2 2000-06-04 06:34:54 unix01 Exp $
  */
 
 
 public class Chandec extends Absyn implements Serializable { 
+  public String chan;    // Name als String
+
+  public Chandec (String s) {chan = s;};
 }
 
 
@@ -20,9 +23,12 @@ public class Chandec extends Absyn implements Serializable {
 //	Abstract Syntax for Mist Programs
 //	------------------------------------
 //
-//	$Id: Chandec.java,v 1.1 2000-05-28 15:11:15 unix01 Exp $
+//	$Id: Chandec.java,v 1.2 2000-06-04 06:34:54 unix01 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.1  2000/05/28 15:11:15  unix01
+//	ok
+//	
 //	
 //---------------------------------------------------------------------
 
