910d499f5f04a4265a9b4352343082b0e6c12ddb
unix01 <msteffen@ifi.uio.no>
2000-06-04 11:39:16 +0000

    *** empty log message ***

---
+++ src/utils/PrettyPrint.java
@@ -0,0 +1,14 @@
+/** Ein einfacher Pretty-Printer fuer die  abstrakte Syntax.
+
+
+ * @author Initially provided by Martin Steffen.
+ * @version  $Id: PrettyPrint.java,v 1.1 2000-06-04 11:39:16 unix01 Exp $    
+ */
+    
+
+package utils;
+import absynt.*;
+
+public class PrettyPrint {
+  
+}
