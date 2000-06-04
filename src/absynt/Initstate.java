3d074a303db525790aaee7b0fabb03aba5086028
unix01 <msteffen@ifi.uio.no>
2000-06-04 09:02:03 +0000

    Konstruktor + Felder entsprechend dem Pflichtenheft hinzugefuegt

--- src/absynt/Initstate.java
+++ src/absynt/Initstate.java
@@ -6,12 +6,18 @@ import java.io.Serializable;
  * Initialer Zustand
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: Initstate.java,v 1.2 2000-05-28 12:57:10 unix01 Exp $
+ * @version $Id: Initstate.java,v 1.3 2000-06-04 09:02:03 unix01 Exp $
  */
 
 
 public class Initstate  extends  Astate implements Serializable { 
-
+  public String name;
+  public Expr   assert;
+  
+  public Initstate (String s, Expr e) {
+    name     = s;
+    assert   = e;
+  };
 }
 
 
@@ -21,9 +27,12 @@ public class Initstate  extends  Astate implements Serializable {
 //	Abstract Syntax for Mist Programs
 //	------------------------------------
 //
-//	$Id: Initstate.java,v 1.2 2000-05-28 12:57:10 unix01 Exp $
+//	$Id: Initstate.java,v 1.3 2000-06-04 09:02:03 unix01 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  2000/05/28 12:57:10  unix01
+//	Zwischenzustand, vor Reorganisation
+//	
 //	Revision 1.1  2000/05/28 11:11:25  unix01
 //	ok
 //	
