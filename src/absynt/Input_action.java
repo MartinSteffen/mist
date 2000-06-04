348e1826cd5130853643ff1127b1e74a83c94165
unix01 <msteffen@ifi.uio.no>
2000-06-04 09:40:11 +0000

    Fehler ausgebessert

--- src/absynt/Input_action.java
+++ src/absynt/Input_action.java
@@ -6,17 +6,17 @@ import java.io.Serializable;
  * Kommunikation: Eingabe
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: Input_action.java,v 1.3 2000-06-04 09:36:28 unix01 Exp $
+ * @version $Id: Input_action.java,v 1.4 2000-06-04 09:40:11 unix01 Exp $
  */
 
 
 public class Input_action  extends Action implements Serializable { 
-  public Channel chan;
-  public Expr    val;
+  public Channel  chan;
+  public Variable  var;
 
-  public Input_action (Channel c, Expr e) {
+  public Input_action (Channel c, Variable x) {
     chan = c;
-    val  = e;
+    var  = x;
   };
 }
 
@@ -27,9 +27,12 @@ public class Input_action  extends Action implements Serializable {
 //	Abstract Syntax for Mist Programs
 //	------------------------------------
 //
-//	$Id: Input_action.java,v 1.3 2000-06-04 09:36:28 unix01 Exp $
+//	$Id: Input_action.java,v 1.4 2000-06-04 09:40:11 unix01 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.3  2000/06/04 09:36:28  unix01
+//	Kontruktor/Felder entsprechend des Pflichtenhefts hinzugefuegt
+//	
 //	Revision 1.2  2000/05/28 12:57:10  unix01
 //	Zwischenzustand, vor Reorganisation
 //	
