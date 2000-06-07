c9a44ed523c4bf2f79a48d14fadcd4184738ec89
unix01 <msteffen@ifi.uio.no>
2000-06-07 14:11:51 +0000

    Nur zur Demo

--- src/absynt/State.java
+++ src/absynt/State.java
@@ -2,11 +2,14 @@ package absynt;
 import java.io.Serializable;
 
 
+
+
+
 /**
  * Zustand (au"ser initialem Zustand)
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: State.java,v 1.3 2000-06-04 09:04:59 unix01 Exp $
+ * @version $Id: State.java,v 1.4 2000-06-07 14:11:51 unix01 Exp $
  */
 
 
@@ -28,9 +31,12 @@ public class State extends  Astate implements Serializable {
 //	Abstract Syntax for Mist Programs
 //	------------------------------------
 //
-//	$Id: State.java,v 1.3 2000-06-04 09:04:59 unix01 Exp $
+//	$Id: State.java,v 1.4 2000-06-07 14:11:51 unix01 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.3  2000/06/04 09:04:59  unix01
+//	Konstruktor/Felder entsprechend dem Pflichtenheft hinzugefuegt
+//	
 //	Revision 1.2  2000/05/28 12:57:12  unix01
 //	Zwischenzustand, vor Reorganisation
 //	
