546142f6607762176010d88f72e7bc2a8a192786
unix01 <msteffen@ifi.uio.no>
2000-06-19 17:17:38 +0000

    -  Zus"atzliche "uberladene Konstruktoren eingef"ugt
    -  Felder f"ur State/Initstate nach  Astate verschoben.

--- src/absynt/Initstate.java
+++ src/absynt/Initstate.java
@@ -6,17 +6,20 @@ import java.io.Serializable;
  * Initialer Zustand
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: Initstate.java,v 1.3 2000-06-04 09:02:03 unix01 Exp $
+ * @version $Id: Initstate.java,v 1.4 2000-06-19 17:17:38 unix01 Exp $
  */
 
 
 public class Initstate  extends  Astate implements Serializable { 
-  public String name;
-  public Expr   assert;
   
   public Initstate (String s, Expr e) {
     name     = s;
     assert   = e;
+  };
+  public Initstate (String s, Expr e, Position p) {
+    name     = s;
+    assert   = e;
+    pos      = p;
   };
 }
 
@@ -27,9 +30,12 @@ public class Initstate  extends  Astate implements Serializable {
 //	Abstract Syntax for Mist Programs
 //	------------------------------------
 //
-//	$Id: Initstate.java,v 1.3 2000-06-04 09:02:03 unix01 Exp $
+//	$Id: Initstate.java,v 1.4 2000-06-19 17:17:38 unix01 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.3  2000/06/04 09:02:03  unix01
+//	Konstruktor + Felder entsprechend dem Pflichtenheft hinzugefuegt
+//	
 //	Revision 1.2  2000/05/28 12:57:10  unix01
 //	Zwischenzustand, vor Reorganisation
 //	
--- src/absynt/State.java
+++ src/absynt/State.java
@@ -9,17 +9,21 @@ import java.io.Serializable;
  * Zustand (au"ser initialem Zustand)
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: State.java,v 1.4 2000-06-07 14:11:51 unix01 Exp $
+ * @version $Id: State.java,v 1.5 2000-06-19 17:17:38 unix01 Exp $
  */
 
 
 public class State extends  Astate implements Serializable { 
-  public String name;
-  public Expr   assert;
   
   public State (String s, Expr e) {
     name     = s;
     assert   = e;
+    pos = null;
+  };  
+  public State (String s, Expr e, Position p) {
+    name     = s;
+    assert   = e;
+    pos      = p;
   };  
 
 }
@@ -31,9 +35,12 @@ public class State extends  Astate implements Serializable {
 //	Abstract Syntax for Mist Programs
 //	------------------------------------
 //
-//	$Id: State.java,v 1.4 2000-06-07 14:11:51 unix01 Exp $
+//	$Id: State.java,v 1.5 2000-06-19 17:17:38 unix01 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.4  2000/06/07 14:11:51  unix01
+//	Nur zur Demo
+//	
 //	Revision 1.3  2000/06/04 09:04:59  unix01
 //	Konstruktor/Felder entsprechend dem Pflichtenheft hinzugefuegt
 //	
