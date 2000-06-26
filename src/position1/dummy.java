b23d3deefd0986604ff2d2daf01ce51b48d8a5cf
unix01 <msteffen@ifi.uio.no>
2000-06-26 13:56:40 +0000

    Neu: Makefile, dummy-code (dummy.java)

---
+++ src/position1/dummy.java
@@ -0,0 +1,16 @@
+/**
+Eine Dummy-Funktion, die einen gegebenen Process ungesehen zurueckgibt.
+Nur zum Testen.
+**/
+
+
+package position1;
+
+import absynt.Process;
+
+public class dummy implements Position {
+
+public void positioniere(Process process) {}   
+
+
+}
