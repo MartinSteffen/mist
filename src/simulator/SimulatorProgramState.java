b198b40c2a8ad34ae730682500eadee31cc08554
unix01 <msteffen@ifi.uio.no>
2000-07-03 12:06:54 +0000

    Klasse für die Repräsentation eines Programmzustands, noch nicht fertig, noch nicht compilierbar

---
+++ src/simulator/SimulatorProgramState.java
@@ -0,0 +1,29 @@
+package simulator;
+
+
+
+/**
+ * class SimulatorProgramState - Klasse die den Zustand eines im Ablauf befindlichen
+ * <b>Mist-Programms</b> modelliert.
+ * @author Michael Gömann
+ * @author Michael Nimser 
+ * @version  1.0, 07/03/2000
+ */
+
+
+public class SimulatorProgramState {
+    
+    /** Instanzfeld für die Prozesszustände eines Programms als Array
+     */
+    SimulatorProcessState[] processStateList;
+
+    /** Konstruktor zur Generierung eines Objektes, das den Programmzustand 
+     * wiederspiegeln können soll. 
+     */
+
+    SimulatorProgramState (int numberOfProcesses) {
+	processStateList = new SimulatorProcessState[numberOfProcesses];
+    }
+
+
+}
