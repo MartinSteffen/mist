75b9edd944d30c4a91c5ca70fa123cb01a7d76e3
unix01 <msteffen@ifi.uio.no>
2000-07-09 18:35:06 +0000

    die Methode parseExpression(java.lang.String als stub hinzugefügt javadoc Kommentare hinzugefügt log aktiviert

--- src/parser/ParseException.java
+++ src/parser/ParseException.java
@@ -1,13 +1,17 @@
 package parser;
 
 /**
- * Exception-Class to provide some information about parse-errors.
+ * Exception-Class to provide some information about parse-errors.<br><br>
  * 
  * Exception-Class um Informationen an die aufrufenden Klassen
  * weiterzuleiten.
  *
  * @author Andreas Scott, Alexander Hegener (unix13)
- * @version $Id: ParseException.java,v 1.2 2000-06-07 13:13:04 unix01 Exp $
+ * @version $Id: ParseException.java,v 1.3 2000-07-09 18:35:05 unix01 Exp $
+ *
+ * <!-- Log=====
+ * $Log: not supported by cvs2svn $
+ * ============= //-->
  */
 
 public class ParseException extends Exception{
@@ -17,14 +21,16 @@ public class ParseException extends Exception{
      * Default-Konstruktor, erzeugt Exception mit Fehler: "syntax error"
      */
     ParseException(){
-	super("syntax error");
+		super("syntax error");
     }
-
+	
     /**
      * Konstruktor, erzeugt Exception mit Fehler errString
+	 * @parameter 
+	 * - errString: Nachricht
      */
     ParseException(String errString){
-	super(errString);
+		super(errString);
     }
 
 }
