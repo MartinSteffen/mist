ccd1014e5bb53a1787e6c8a9475f307421b7cad3
unix01 <msteffen@ifi.uio.no>
2000-07-10 17:41:09 +0000

    Variablendeklaration an die Vereinbarungen/"Anderungen im Pflichtenheft angepasst. => Typ hinzugefuegt.
    Die alten Konstruktoren zeitweise zur Kompatibilit"at
    beibehalten.
    
    [Steffen]

--- src/absynt/Vardec.java
+++ src/absynt/Vardec.java
@@ -6,31 +6,44 @@ import java.io.Serializable;
  * Variablendeklaration.
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: Vardec.java,v 1.5 2000-07-10 14:55:33 unix01 Exp $
+ * @version $Id: Vardec.java,v 1.6 2000-07-10 17:41:09 unix01 Exp $
  */
 
 
 public class Vardec extends Absyn implements Serializable { 
 
-  public Variable var;
-  public Expr     val = null;    // Kann null sein
+  public Variable   var;
+  public Expr       val = null;    // Wir werden verlangen, da"s es nicht null sein darf
+  public M_AtomType mype;          // Wir werden verlangen, da"s es immer gesetzt ist
 
 
   /**
-   * Zwei Konstruktoren: einer fuer die reine Deklaration,
-   * eine, falls man die Variable deklariert und gleich mit
-   * einem Wert fuellt. 
+   * Momentan 3 Konstruktoren. Am Ende ist folgender
+   * nur noch ein Konstruktor vorgesehen (nach Diskussion mit)
+   * der Modelchecker/Simulatorgruppe:
    *
-   * Typen sind (noch) nicht vorgesehen, das haengt davon
-   * abstract, ob wir eine Checker-Gruppe bekommen.
+   *   public Vardec (Variable x, Constval e, M_AtomType _t) {
+   *
+   *   Aus Gruenden der tempor"aren Kompilierbarkeit werden die Konstruktoren
+   *   fuer ein paar Tage noch uberladen beibehalten,
    */
 
+  public Vardec (Variable x, Expr e, M_AtomType _t) {
+    var = x;
+    val = e;
+    type = _t;
+  };
+
+
+  
+  // diese Methode soll am Ende nicht mehr Unterst"utzt werden
   public Vardec (Variable x, Expr e) {
     var = x;
     val = e;
   };
 
 
+  // diese Methode soll am Ende nicht mehr Unterst"utzt werden
   public Vardec (Variable x) {
     var = x;
   };
@@ -61,9 +74,18 @@ public class Vardec extends Absyn implements Serializable {
 //	Abstract Syntax for Mist Programs
 //	------------------------------------
 //
-//	$Id: Vardec.java,v 1.5 2000-07-10 14:55:33 unix01 Exp $
+//	$Id: Vardec.java,v 1.6 2000-07-10 17:41:09 unix01 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.5  2000/07/10 14:55:33  unix01
+//	Neue Klassen fuer die Abstrakte Syntax hinzugefuegt, fuer die
+//	typisierung:
+//		     Type
+//		    /   \
+//	      AtomType  Chan
+//	      /    \
+//	     Bool   Int
+//	
 //	Revision 1.4  2000/07/10 14:41:39  unix01
 //	ok
 //	
