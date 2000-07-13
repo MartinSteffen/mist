84070b83a9cd7094ecfde6cc9e9153b994106c44
unix01 <msteffen@ifi.uio.no>
2000-07-13 08:29:27 +0000

    Typ wieder geloescht, gehoert wohl zur Klasse ChanDec

--- src/absynt/Channel.java
+++ src/absynt/Channel.java
@@ -6,21 +6,15 @@ import java.io.Serializable;
  * Klasse fuer Kanalnamen. Im wesentlichen ein Wrapper fuer Strings
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: Channel.java,v 1.2 2000-07-12 17:01:56 unix01 Exp $
+ * @version $Id: Channel.java,v 1.3 2000-07-13 08:29:27 unix01 Exp $
  */
 
 
 public class Channel extends Absyn implements Serializable { 
 	public String name ;
-	public M_Type type ;
   
 	public Channel (String s) {
 		name = s;};
-
-	public Channel (String s, M_Type t){
-		name = s;
-		type = t;
-	}
 
 }
 
@@ -31,9 +25,12 @@ public class Channel extends Absyn implements Serializable {
 //	Abstract Syntax for Mist Programs
 //	------------------------------------
 //
-//	$Id: Channel.java,v 1.2 2000-07-12 17:01:56 unix01 Exp $
+//	$Id: Channel.java,v 1.3 2000-07-13 08:29:27 unix01 Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.2  2000/07/12 17:01:56  unix01
+//	Typ hinzugefuegt (und somit auch zusaetzlichen Konstruktor)
+//	
 //	Revision 1.1  2000/06/04 09:35:21  unix01
 //	Klasse fuer Kanalnamen neu hinzugefuegt. Die Klasse
 //	war _nicht_ im originalen Syntaxvorschlag!
