7087781f21a59d8120bf48e269676ee7f788ed27
unix01 <msteffen@ifi.uio.no>
2000-07-14 05:58:16 +0000

    Schnittstelle parseExpression(java.lang.String expression) mit Code versehen, funktioniert soweit und kann in den Editor eingebunden werden [Scott]

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -2,17 +2,21 @@ package parser;
 
 import java.io.IOException;
 import java.io.FileInputStream;
+import java.io.StringReader;
 
 /**
  * Stellt die Methoden parseFile und parseException der
  * Oeffentlichkeit zur Verfuegung
  *
  * @author Andreas Scott &amp; Alexander Hegener 
- * @version $Id: Parser.java,v 1.5 2000-07-12 20:47:20 unix01 Exp $
+ * @version $Id: Parser.java,v 1.6 2000-07-14 05:58:16 unix01 Exp $
  * @see <a href="../../Scanner.lex">Scanner.lex</a>
  * @see Scanner
  * <!-- Log ==========
  * $Log: not supported by cvs2svn $
+ * Revision 1.5  2000/07/12 20:47:20  unix01
+ * parsen der Statements (debugged) (Alexander)
+ *
  * Revision 1.4  2000/07/11 11:39:17  unix01
  * erstmalig eingefuehrt (nicht ganz, aber doch schon)
  *
@@ -62,11 +66,15 @@ public class Parser{
    * @return 
    * ein Ausdruck wie absynt.Expr
    * @throws 
-   * parser.ParseException falls ein Syntax-Fehler vorliegt. Der Rueckgabe wert ist dann <b>null</b>
+   * parser.ParseException falls ein Syntax-Fehler vorliegt. 
    */
   public absynt.Expr parseExpression(java.lang.String expression) 
-	  throws ParseException{
-	  return null;
+	  throws ParseException, IOException{ 
+	  absynt.Expr ausdruck = null;
+	  parser.Scanner scanner = new Scanner (new StringReader(expression+";"));
+	  scanner.token = scanner.nextToken();
+	  ausdruck = scanner.buildExpression();
+	  return ausdruck;
   }
 
 
