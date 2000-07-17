087aff746a4298a8a910641e21553ededd691ee6
unix01 <msteffen@ifi.uio.no>
2000-07-17 14:10:20 +0000

    Umstellung auf cup1.0k

--- src/parser/FileParser.java
+++ src/parser/FileParser.java
@@ -7,9 +7,12 @@ import java.io.*;
 /**
  * Zum Testen des Parsers mit Dateien
  * @author Andreas Scott &amp; Alexander Hegener
- * @version $Id: FileParser.java,v 2.0 2000-07-17 10:45:21 unix01 Exp $
+ * @version $Id: FileParser.java,v 2.1 2000-07-17 14:10:19 unix01 Exp $
  * <-- ================
  * $Log: not supported by cvs2svn $
+ * Revision 2.0  2000/07/17 10:45:21  unix01
+ * Umstellung auf CUP
+ *
  * Revision 1.1  2000/07/14 06:09:32  unix01
  * Klasse um den Parser zu testen [Scott]
  *
@@ -31,7 +34,11 @@ class FileParser{
 		  System.err.println(e.toString());
 		  System.exit(1);
 	  } catch (IOException e){
+		  System.err.println(e.toString());
 		  System.exit(1);
+	  } catch (Exception e){
+	      System.err.println(e.toString());
+	      System.exit(1);
 	  }
   }
 }
